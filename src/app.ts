f353443642c6715344e1cfe38ab1206c589d8551
ClementDidom <clement@freshplanet.com>
2020-06-09 01:20:43 -0400

    feature: added cors and health check

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import * as express from 'express';
+import * as cors from 'cors';
 
 const app = express();
 
-app.get('/', (_, res) => res.send('Hello World!'));
+app.use(cors());
+
+app.get('/healthcheck', (_, res) => {
+  res.status(200).send({
+    status: 'up',
+    version: '0.0.1',
+    environment: process.env.NODE_ENV
+  });
+});
 
 export { app };
